8a02046cea21b007e42382e74b39085d51ed14e9
Patrik Smolíček <patrik.smolicek@sosholic.sk>
2024-03-14 12:55:59 +0100

    fix

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,8 +3,6 @@ module com.example.smolicekcinema {
     requires javafx.fxml;
 
 
-    opens com.example.smolicekcinema to javafx.fxml;
-    exports com.example.smolicekcinema;
     exports patrik.smolicek.cinema;
     opens patrik.smolicek.cinema to javafx.fxml;
 }
